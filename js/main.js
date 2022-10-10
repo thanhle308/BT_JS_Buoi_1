f4d0ff3a73f0e153dd06962e0f6446bd4454c655
ThanhLe <lechithanh.devops@gmail.com>
2022-10-10 10:45:25 +0700

    Hoan thanh

--- js/main.js
+++ js/main.js
@@ -68,3 +68,56 @@ function quydoi(){
 }
 
 document.getElementById("btndoi").onclick = quydoi;
+
+// Bai 4
+/* ----------------------------- Mô hình 3 khối ----------------------------- */
+// Khối 1: input (dữ liệu được cung cấp)
+    // Nhập số chiều dai và rộng
+
+// Khối 2: Các bước xử lý 
+//     B1: Khai báo biến và lưu giá trị input
+//     B2:Lập công thức tính toán
+//     Diện tích = dài * rộng
+//     Chu vi = (dài +rộng)*2
+// Khối 3: Output (kết quả đạt được)
+//     Diện tích và Chu vi
+
+function tinh(){
+    var dai = document.getElementById("dai").value;
+    var rong = document.getElementById("rong").value;
+
+    var dientich = (Number(dai))*(Number(rong));
+    var chuvi = ((Number(dai))+(Number(rong)))*2;
+
+    document.getElementById("txtdairong").innerHTML = "Diện tích : " + dientich +", Chu vi : " + chuvi;
+}
+
+document.getElementById("btntinh").onclick = tinh;
+
+// Bai 5
+/* ----------------------------- Mô hình 3 khối ----------------------------- */
+// Khối 1: input (dữ liệu được cung cấp)
+    // Nhập số có 2 chữ số
+
+// Khối 2: Các bước xử lý 
+//     B1: Khai báo biến và lưu giá trị input
+//     B2:Lập công thức tính toán
+//     Số hàng đơn vị = số %10
+//     Số hàng chục = số / 10
+//     Tổng 2 ký số = số hàng đơn vị + số hàng chục
+// Khối 3: Output (kết quả đạt được)
+//     Tổng 2 ký số
+
+function tong2kyso(){
+    var so = document.getElementById("so").value;
+
+    var so_hang_dv = (Number(so) % 10);
+    // parseInt ép kiểu số thực 
+    var so_hang_chuc = parseInt((Number(so) / 10));
+    var tong = so_hang_dv + so_hang_chuc;
+   
+
+    document.getElementById("txt2so").innerHTML = tong;
+}
+
+document.getElementById("btntinh2so").onclick = tong2kyso;
